1ff013190ce3cb31d52c425e4c575c562ce8e113
Kailash-Shankar <kailashs2006@gmail.com>
2025-10-30 18:07:05 -0400

    RB Tree with rebalancing

--- Backend/Red-Black.cpp
+++ Backend/Red-Black.cpp
@@ -58,16 +58,38 @@ class Red_Black{
 
     }
 
-    Node* RotateLeft(Node* root){
-        Node* temp = root->parent->left; 
-        root->parent->left = root->parent->parent;
-        root->parent->parent->right = temp;
+    Node* RotateLeft(Node*& node){
+        Node* temp = node->right;
+        node->right = temp->left;    // Link node to temp->left
+        if (temp->left) temp->left->parent = node; // Link temp->left to node
+        temp->parent = node->parent;
+        if (!node->parent) {
+           this->root = temp;
+        } else if (node->parent->left == node){ // Node is a left child
+            node->parent->left = temp;
+        } else { // Node is a right child
+            node->parent->right = temp;
+        }
+        temp->left = node;   // Connect node to temp
+        node->parent = temp;
+        return temp;
     }
 
-    Node* RotateRight(Node* root){
-        Node* temp = root->parent->right; 
-        root->parent->right = root->parent->parent;
-        root->parent->parent->left = temp;
+    Node* RotateRight(Node*& node){
+      Node* temp = node->left;
+        node->left = temp->right;    // Link root to temp->left
+        if (temp->right) temp->right->parent = node; // Link temp->left to root
+        temp->parent = node->parent;
+        if (!node->parent) {
+           this->root = temp;
+        } else if (node->parent->right == node){ // Node is a left child
+            node->parent->right = temp;
+        } else { // Node is a right child
+            node->parent->left = temp;
+        }
+        temp->right = node;   // Connect node to temp
+        node->parent = temp;
+        return temp;
     }
 
     void insert(int zip, vector<string> info, float price){
@@ -96,40 +118,62 @@ class Red_Black{
 
         else {return root;} //Unlikely to happen in data
         
-        //Rebalancing
+        //Rebalancing------------------------------------------------------------------------------------------------------
         
-        bool parentIsLeftChild = false;
+      
         
-        if (root->parent && root->parent->parent && root->parent->parent->left == root->parent){
-            parentIsLeftChild = true;
-        }
-
         
-        if (IsRed(root) && IsRed(root->parent)){  // Check for consecutive red nodes - current and parent are red
-            if (!parentIsLeftChild){   // Left uncle case
-                if (IsRed(root->parent->parent->left)){ // Uncle is red
-                    setcolor(root->parent, 0);
-                    setcolor(root->parent->parent, 1);
-                    setcolor(root->parent->parent->left, 0);
+        
+        Node* Parent = root->parent;
+        Node* Grandparent = Parent ? Parent->parent : nullptr;
+        if (!Parent || !Grandparent) return root;
+        
+        
+        if (IsRed(root) && IsRed(Parent)){  // Check for consecutive red nodes - current and parent are red
+            if (Grandparent->right == Parent){   // Left uncle case
+                Node* Uncle = Grandparent->left;
+                if (IsRed(Uncle)){ // Uncle is red
+                    setcolor(Parent, 0);
+                    setcolor(Grandparent, 1);
+                    setcolor(Uncle, 0);
+                   
                 } else {                                // Uncle is black
-                   root = RotateLeft(root);
+                   if (Parent->left == root){            // Right-Left Case
+                    RotateRight(Parent);    
+                  
+                   }
+                   RotateLeft(Grandparent);              // Right-Right Case
+                   setcolor(Parent, 0);
+                   setcolor(Grandparent, 1);
+                 
                     
                 }
                     
             }
-            else if (parentIsLeftChild && IsRed(root->parent->parent->right)){  // Right uncle case
-                if (IsRed(root->parent->parent->right)){ // Uncle is red
-                    setcolor(root->parent, 0);
-                    setcolor(root->parent->parent, 1);
-                    setcolor(root->parent->parent->right, 0);
+            else {  // Right uncle case
+                Node* Uncle = Grandparent->right;
+                if (IsRed(Uncle)){ // Uncle is red
+                    setcolor(Parent, 0);
+                    setcolor(Grandparent, 1);
+                    setcolor(Uncle, 0);
+                    
                 } else {                                // Uncle is black
-                    root = RotateRight(root);
-                }
-        
+                   if (Parent->right == root){            // Left-Right Case
+                    RotateLeft(Parent);    
+                    
+                   }
+                   RotateRight(Grandparent);              // Left-left Case
+                   setcolor(Parent, 0);
+                   setcolor(Grandparent, 1);
+                 
+                    
+                
+                    
         
         
             } 
-        }
+        } 
+    }
         
 
        return root;
@@ -181,10 +225,14 @@ class Red_Black{
 };
 
 int main(){
+    //Testing
     Red_Black tree;
     vector<string> v = {"a", "b", "c", "d"};
     tree.insert(5, v, 2.5); 
     tree.insert(7, v, 44.0); 
+     tree.insert(1, v, 44.0); 
+      tree.insert(2, v, 44.0); 
+       tree.insert(63, v, 44.0); 
     Node* r = tree.search(7);
     cout << r->price << endl;
 
