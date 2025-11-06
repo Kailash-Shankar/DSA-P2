d8eba090e8221626a463d89eef05ae1a44a039a0
Kailash-Shankar <kailashs2006@gmail.com>
2025-11-05 21:17:08 -0500

    small edits

--- Backend/Data.cpp
+++ Backend/Data.cpp
@@ -135,52 +135,50 @@ cout << "Time taken: " << duration.count() << " seconds" << endl;
 httplib::Server svr;
     
     
-    svr.Get(R"(/node/(\d+))", [](const httplib::Request& req, httplib::Response& res) {
-        
-        int zip = stoi(req.matches[1].str());
-        
-        cout << "Request for zip: " << zip << endl;
-        
-       
-        double dur = 0;
-        vector<string> result = fetch_data(zip, dur);
-        
-        if (result.empty()) {
-            // Zip code not found
-            res.status = 404;
-            res.set_content(R"({"error": "Zip code not found"})", "application/json");
-            return;
-        }
-        
-        // Build JSON response manually
-        stringstream json;
-        json << "{";
-        json << "\"price\": " << result[0] << ",";
-        json << "\"time\": " << fixed << setprecision(6) << dur << ",";
-        json << "\"info\": [";
-        for (size_t i = 1; i < result.size(); i++) {
-            json << "\"" << result[i] << "\"";
-            if (i < result.size() - 1) json << ",";
-        }
-        json << "]}";
-        
-        // Set CORS headers (allows React to call this)
-        res.set_header("Access-Control-Allow-Origin", "*");
-        res.set_content(json.str(), "application/json");
-    });
+svr.Get(R"(/node/(\d+))", [](const httplib::Request& req, httplib::Response& res) {
     
-    // Handle OPTIONS requests for CORS
-    svr.Options(R"(/node/(\d+))", [](const httplib::Request&, httplib::Response& res) {
-        res.set_header("Access-Control-Allow-Origin", "*");
-        res.set_header("Access-Control-Allow-Methods", "GET, OPTIONS");
-        res.set_header("Access-Control-Allow-Headers", "Content-Type");
-    });
+    int zip = stoi(req.matches[1].str());
     
-    // Start server
-    cout << "Server running on http://localhost:8080" << endl;
-    cout << "Try: http://localhost:8080/node/77494" << endl;
+    cout << "Request for zip: " << zip << endl;
     
-    svr.listen("0.0.0.0", 8080);
+    
+    double dur = 0;
+    vector<string> result = fetch_data(zip, dur);
+    
+    if (result.empty()) {
+        res.status = 404;
+        res.set_content(R"({"error": "Zip code not found"})", "application/json");
+        return;
+    }
+    
+    
+    stringstream json;
+    json << "{";
+    json << "\"price\": " << result[0] << ",";
+    json << "\"time\": " << fixed << setprecision(6) << dur << ",";
+    json << "\"info\": [";
+    for (size_t i = 1; i < result.size(); i++) {
+        json << "\"" << result[i] << "\"";
+        if (i < result.size() - 1) json << ",";
+    }
+    json << "]}";
+    
+    
+    res.set_header("Access-Control-Allow-Origin", "*");
+    res.set_content(json.str(), "application/json");
+});
+
+
+svr.Options(R"(/node/(\d+))", [](const httplib::Request&, httplib::Response& res) {
+    res.set_header("Access-Control-Allow-Origin", "*");
+    res.set_header("Access-Control-Allow-Methods", "GET, OPTIONS");
+    res.set_header("Access-Control-Allow-Headers", "Content-Type");
+});
+
+cout << "Server running on http://localhost:8080" << endl;
+cout << "Try: http://localhost:8080/node/77494" << endl;
+
+svr.listen("0.0.0.0", 8080);
     
 
 return 0;
