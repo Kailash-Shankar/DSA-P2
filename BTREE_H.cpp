fd8e9a40646ba4bd00e7cad373e65b823eefc341
aryanm23 <aryanmukherjee2006@gmail.com>
2025-11-06 07:27:19 -0500

    B-Tree search and delete

---
+++ BTREE_H.cpp
@@ -0,0 +1,82 @@
+#ifndef BTREE_H
+#define BTREE_H
+
+#include <vector>
+#include <string>
+
+class BTree{
+public:
+    struct Record{
+        int zip;
+        double price;
+        std::string info;
+    };
+
+    //Constructor
+    BTree(int minDegree) : root(nullptr), t(minDegree)
+    {}
+
+    //destructor
+    ~BTree() {
+        clear(root);
+    }
+    }
+
+    //checking if zip exists in the tree
+    bool search(int zip) const{
+        const Record* res=search(root, zip);
+        return(res!=nullptr);
+    }
+
+    const Record* find(int zip) const{
+        return search(root, zip);
+    }
+
+private:
+    struct Node {
+        bool leaf;
+        std::vector<Record> records;     
+        std::vector<Node*> children;
+
+        Node(bool isLeaf, int minDegree) : leaf(isLeaf)
+        {
+            //this is just to avoid reallocation 
+            records.reserve(2*minDegree-1);
+            children.reserve(2*minDegree);
+        }
+    };
+
+    Node* root;
+    int t; //min degree
+
+    //deletion of nodes
+    void clear(Node* node){
+        if(node==nullptr) return;
+
+        for(Node* child : node->children){
+            clear(child);
+        }
+
+        delete node;
+    }
+
+    //helper for search
+    const Record* search(Node* node, int zip) const {
+        if (node==nullptr) return nullptr;
+
+        int i=0;
+        while (i<(int)node->records.size() && zip>node->records[i].zip) 
+        { ++i; }
+
+        if(i<(int)node->records.size() && zip==node->records[i].zip) {
+            return &node->records[i];
+        }
+
+        if (node->leaf) { //not in tree
+            return nullptr; 
+        }
+
+        return search(node->children[i], zip);
+    }
+
+#endif //BTREE_H
